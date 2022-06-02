0faad08ae740086e9fa0b8ae4cd8cb744abac341
prateek <stupidcoder22@gmail.com>
2022-06-02 19:06:35 +0530

    setup done

---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className={`nav`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avtar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+};
+
+export default Header;
